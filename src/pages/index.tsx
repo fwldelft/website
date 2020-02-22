577969eca6ef3af75be8400facc16b1d4bf65d34
Carlos Precioso <kalprestito@gmail.com>
2020-02-22 19:54:48 +0100

    Add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import { jsx } from "@emotion/core"
 import { NextPage } from "next"
+import Head from "next/head"
 import { LandingSection } from "../components/Sections/Landing"
 
-const IndexPage: NextPage = () => <LandingSection />
+const IndexPage: NextPage = () => (
+  <>
+    <Head>
+      <title>FWL Delft 2020</title>
+    </Head>
+    <LandingSection />
+  </>
+)
 
 export default IndexPage
