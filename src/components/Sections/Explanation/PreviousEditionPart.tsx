586c124de4fbdc34cbd8e2bf9eb16911ca3933a3
Carlos Precioso <kalprestito@gmail.com>
2020-03-04 13:26:29 +0100

    Add subtitles for 2019 jury

--- src/components/Sections/Explanation/PreviousEditionPart.tsx
+++ src/components/Sections/Explanation/PreviousEditionPart.tsx
@@ -20,20 +20,41 @@ export const PreviousEditionPart: FunctionComponent = () => (
     </Subsection>
 
     <Subsection title="Jury">
-      <JudgeBox name="Marc Kalf" image="/static/jury-images/kalf.jpg" />
-      <JudgeBox name="Bart Ahsmann" image="/static/jury-images/ahsmann.png" />
+      <JudgeBox
+        name="Marc Kalf"
+        subtitle="Health Valley"
+        image="/static/jury-images/kalf.jpg"
+        link="https://www.linkedin.com/in/marckalf/"
+      />
+      <JudgeBox
+        name="Bart Ahsmann"
+        subtitle="CLICKNL"
+        image="/static/jury-images/ahsmann.png"
+        link="https://www.linkedin.com/in/bartahsmann/"
+      />
       <JudgeBox
         name="Paul van de Laar"
+        subtitle="Museum Rotterdam"
         image="/static/jury-images/vdlaar.jpg"
+        link="https://www.linkedin.com/in/paul-van-de-laar-89298323/"
       />
-      <JudgeBox name="Laura de Troia" image="/static/jury-images/detroia.jpg" />
+      <JudgeBox
+        name="Laura de Troia"
+        subtitle="SouthEataly"
+        image="/static/jury-images/detroia.jpg"
+        link="https://www.linkedin.com/in/laura-de-troia-412a9aa2/"
+      />
       <JudgeBox
         name="Francesca Grazian"
+        subtitle="TU Delft"
         image="/static/jury-images/grazian.jpg"
+        link="https://www.linkedin.com/in/francescagrazian/"
       />
       <JudgeBox
         name="Anouk de Ruiter"
+        subtitle="TEDx Delft"
         image="/static/jury-images/deruiter.jpg"
+        link="https://www.linkedin.com/in/anouk-de-ruiter-728b97b/"
       />
     </Subsection>
   </Section>
