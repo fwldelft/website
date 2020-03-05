ffe5934414b5076d29b154bba79be12302e3a5db
Carlos Precioso <kalprestito@gmail.com>
2020-03-05 13:41:07 +0100

    Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,6 +35,22 @@ const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-159829690-1"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-159829690-1');
+`
+        }}
+      ></script>
     </Head>
     <Global
       styles={css`
