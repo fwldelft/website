55b7be7a01672f66b335f6cd3d6773c14b52e0cd
Carlos Precioso <kalprestito@gmail.com>
2020-06-17 16:33:43 +0200

    Add overflow menu on mobile

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module "!@svgr/webpack!*.svg" {
+  import { FunctionComponent } from "react"
+  const SVG: FunctionComponent
+  export default SVG
+}
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,45 +1,60 @@
+import MenuIcon from "!@svgr/webpack!../img/menu-24px.svg"
 import Link from "next/link"
 import React, { FunctionComponent } from "react"
 import styles from "../css/style.module.css"
 import { cx } from "../util/cx"
 
-export const TopBar: FunctionComponent = () => (
-  <nav className={cx(styles["top-navigation"])}>
-    <div className={cx(styles["wrapped-content"])}>
-      <header>
-        <a href="/">
-          <h1 className={cx(styles["has-menu"])}>
-            Falling Walls Lab
-            <br />
-            <span className={cx(styles["highlight"])}>Delft</span>
-          </h1>
-        </a>
-      </header>
-      <ul className={cx(styles["nav-links"])}>
-        <Link href="/2020">
-          <a>
-            <li>2020 Edition</li>
+let i = 0
+
+export const TopBar: FunctionComponent = () => {
+  const menuOpenId = React.useMemo(() => `__menu-open__${i++}__`, [])
+
+  return (
+    <nav className={cx(styles["top-navigation"])}>
+      <div className={cx(styles["wrapped-content"])}>
+        <header>
+          <a href="/">
+            <h1 className={cx(styles["has-menu"])}>
+              Falling Walls Lab
+              <br />
+              <span className={cx(styles["highlight"])}>Delft</span>
+            </h1>
           </a>
-        </Link>
-        <Link href="/2019">
-          <a>
-            <li>2019 Edition</li>
+        </header>
+        <input
+          id={menuOpenId}
+          className={styles["menu-open"]}
+          type="checkbox"
+        />
+        <label htmlFor={menuOpenId} className={styles["menu-open-label"]}>
+          <MenuIcon />
+        </label>
+        <ul className={cx(styles["nav-links"])}>
+          <Link href="/2020">
+            <a>
+              <li>2020 Edition</li>
+            </a>
+          </Link>
+          <Link href="/2019">
+            <a>
+              <li>2019 Edition</li>
+            </a>
+          </Link>
+          <Link href="/" as="/#about">
+            <a>
+              <li>About</li>
+            </a>
+          </Link>
+          <a href="#contact">
+            <li>Contact</li>
           </a>
-        </Link>
-        <Link href="/" as="/#about">
-          <a>
-            <li>About</li>
-          </a>
-        </Link>
-        <a href="#contact">
-          <li>Contact</li>
-        </a>
-        <Link href="/apply">
-          <a>
-            <li className={cx(styles["accent-button"])}>Apply</li>
-          </a>
-        </Link>
-      </ul>
-    </div>
-  </nav>
-)
+          <Link href="/apply">
+            <a>
+              <li className={cx(styles["accent-button"])}>Apply</li>
+            </a>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  )
+}
