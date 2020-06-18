7d5c6318e2a0bcc280edd29aaadca067f619bb00
Carlos Precioso <kalprestito@gmail.com>
2020-06-18 16:12:18 +0200

    Add styles to stylesheet

--- src/pages/2020.tsx
+++ src/pages/2020.tsx
@@ -1,36 +1,13 @@
 import React, { FunctionComponent } from "react"
-import css from "styled-jsx/css"
 import { ApplyFaqSection } from "../components/ApplyFaqSection"
 import { DetailsSection } from "../components/DetailsSection"
 import { Section, SectionContent } from "../components/Section"
 import styles from "../css/style.module.css"
 import { cx } from "../util/cx"
 
-const { className: coverClassName, styles: coverStyles } = css.resolve`
-  --background-color: var(--red-color);
-  --text-color: var(--white-color);
-  font-weight: bold;
-  text-align: center;
-`
-
 export default (() => (
   <>
-    <style jsx>{`
-      h1 {
-        font-size: 4em;
-      }
-
-      .teaser {
-        grid-column-start: 1;
-        grid-column-end: 5;
-      }
-
-      .teaser-section {
-        text-align: center;
-        display: contents;
-      }
-    `}</style>
-    <Section className={cx(coverClassName)}>
+    <Section className={cx(styles["dry-cover"])}>
       <SectionContent>
         <h1>2020</h1>
         <h2>
@@ -39,16 +16,14 @@ export default (() => (
           Here you can get a small sneak peek.
         </h2>
       </SectionContent>
-
-      {coverStyles}
     </Section>
 
     <DetailsSection />
 
     <Section>
       <SectionContent className={cx(styles["grid"])}>
-        <div className={cx("teaser-section")}>
-          <h2 className={cx("teaser")}>
+        <div className={cx(styles["teaser-section"])}>
+          <h2 className={cx(styles["teaser"])}>
             Come back soon to find out about 2020's...
           </h2>
           <h2>Speakers</h2>
