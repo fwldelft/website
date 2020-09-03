37dbead537a242c20cdf2f7101fb833ff1241227
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2020-09-03 15:13:18 +0200

    Update dates

--- src/data/dates.ts
+++ src/data/dates.ts
@@ -6,12 +6,12 @@ export interface DateDescription {
 
 const dates = {
   application: {
-    date: new Date(2020, 8, 4),
+    date: new Date(2020, 8, 4, 23, 59, 59),
     name: "Application deadline",
     localizedFallback: "Sep 4th, 2020",
   },
   event: {
-    date: new Date(2020, 8, 23),
+    date: new Date(2020, 8, 23, 3, 0, 0),
     name: "FWL Delft 2020",
     localizedFallback: "Sep 23rd, 2020",
   },
